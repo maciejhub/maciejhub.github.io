1662afb0ca5b206c648769486fde99f05e23868c
skibiditoilet <79210631+maciejhub@users.noreply.github.com>
2025-09-05 17:00:01 +0200

    copy code to make better win checking

--- speedrun/script.js
+++ speedrun/script.js
@@ -24,6 +24,17 @@ function starttimer() {
       } 
     } 
     
+    const iframelocationchange = {
+    get value() {
+        return document.getElementById("iframe").contentWindow.location.href;
+    },
+    set value(newValue) {
+        if (document.getElementById("iframe").contentWindow.location.href !== newValue) {
+            diditwin();
+        }
+    }
+};
+
 const urls = ["wiersz", "wideo", "extra", "kasyno", "cytaty", "kalkulator"];
 let permtime = "";
 let win = "false";
